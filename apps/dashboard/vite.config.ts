5270cffc60c1b4874016b26868c73d49b319fcd6
livingitsolutions <livingitsolutions@gmail.com>
2026-03-07 16:56:06 +0800

    Updated vite.config.ts

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -9,5 +9,9 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+    modules: [
+      path.resolve(__dirname, '../../node_modules'),
+      'node_modules',
+    ],
   },
 })
